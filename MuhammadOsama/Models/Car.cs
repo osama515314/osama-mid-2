9cea87b3b0937c266e1cc97aa102d7d32f331453
osama515314 <71315931+osama515314@users.noreply.github.com>
2020-09-15 13:36:01 +0500

    Osama Khan Mid 2

---
+++ MuhammadOsama/Models/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MuhammadOsama.Models
+{
+    public class Car
+    {
+        public int ID {get; set; }
+        public string name {get; set; }
+        public string EngineNumber {get; set; }
+        public string ChasisNumber {get; set; }
+        public string RegNumber {get; set; }
+        public int Price {get; set; }
+
+        internal static IQueryable<Car> Where(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CarDBContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
